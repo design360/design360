b54f3677ea56bd3920449ec6680d58613a127b92
arnavpisces <arnavpisces@gmail.com>
2017-07-22 13:50:59 +0530

    host

--- action.php
+++
@@ -1,9 +0,0 @@
-<?php
- $path = 'sponsors.txt';
- if (isset($_POST['sponsemail'])) {
-    $fh = fopen($path,"a+");
-    $string = $_POST['sponsemail'];
-    fwrite($fh,$string); // Write information to the file
-    fclose($fh); // Close the file
- }
-?>
